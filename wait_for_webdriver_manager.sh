6cfa1104e195b0bc9f272d1b72e8e1728a7e8c5d
Friedrich Clausen <friedrich.clausen@blackboard.com>
2017-08-21 12:31:48 +1000

    Wait for a bit after the port opens

--- wait_for_webdriver_manager.sh
+++ wait_for_webdriver_manager.sh
@@ -5,6 +5,7 @@ while :; do
   # Use some literal character class on the 444[4] to avoid grep matching itself.
   if netstat -tnlp | grep :444[4]; then 
     echo "Started webdriver-manager"
+    sleep 5 # It still seems to need a bit of time after the port opens
     exit 0
   else
     echo "Waiting for webdriver-manger to start listening on port 4444"
